bcaa874250c2fad0e58d7975ddb77220d2a0210b
egrodo <egrodo1@gmail.com>
2018-08-26 18:31:24 -0400

    Added arrow key navigation to date inputs

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -14,6 +14,8 @@ class DatePicker extends Component {
     this.dayInp = React.createRef();
     this.weekInp = React.createRef();
 
+    this.onDayKeyDown = this.onDayKeyDown.bind(this);
+    this.onWeekKeyDown = this.onWeekKeyDown.bind(this);
     this.onDayClick = this.onDayClick.bind(this);
     this.onDayChange = this.onDayChange.bind(this);
     this.onWeekClick = this.onWeekClick.bind(this);
@@ -45,6 +47,34 @@ class DatePicker extends Component {
     }
   }
 
+  onDayKeyDown(e) {
+    if (e.key === 'ArrowDown') {
+      this.setState((({ days }) => {
+        if (days > 0) return { days: (+days) - 1 };
+        return null;
+      }));
+    } else if (e.key === 'ArrowUp') {
+      this.setState((({ days }) => {
+        if (days < 7) return { days: (+days) + 1 };
+        return null;
+      }));
+    }
+  }
+
+  onWeekKeyDown(e) {
+    if (e.key === 'ArrowDown') {
+      this.setState((({ weeks }) => {
+        if (weeks > 0) return { weeks: (+weeks) - 1 };
+        return null;
+      }));
+    } else if (e.key === 'ArrowUp') {
+      this.setState((({ weeks }) => {
+        if (weeks < 9) return { weeks: (+weeks) + 1 };
+        return null;
+      }));
+    }
+  }
+
   submit() {
     // TODO: On submit validate that there is at least one day, then do whatever.
   }
@@ -55,8 +85,10 @@ class DatePicker extends Component {
       <section className="DatePicker">
         <input
           className="dateInp"
+          id="day"
           onClick={this.onDayClick}
           onChange={this.onDayChange}
+          onKeyDown={this.onDayKeyDown}
           ref={this.dayInp}
           value={days}
         />
@@ -65,8 +97,10 @@ class DatePicker extends Component {
         </span>
         <input
           className="dateInp"
+          id="week"
           onClick={this.onWeekClick}
           onChange={this.onWeekChange}
+          onKeyDown={this.onWeekKeyDown}
           ref={this.weekInp}
           value={weeks}
         />
